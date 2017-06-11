0ea1aa366e009d4c92c1e725a464f42a6927d5ad
agix <florian.gaultier@gmail.com>
2017-06-11 22:34:54 +0200

    Dist with version

--- dist/secretin.umd.js
+++ dist/secretin.umd.js
@@ -4,7 +4,7 @@
 	(global.Secretin = factory());
 }(this, (function () { 'use strict';
 
-var version = "1.6.2";
+var version = "1.6.3";
 
 var asyncGenerator = function () {
   function AwaitValue(value) {
